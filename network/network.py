c00d3d235a4a04cd493f82244a712a4866d4c121
uzum <a.uzumcuoglu@gmail.com>
2016-12-18 18:52:37 +0200

    added fixed topology

--- network/network.py
+++ network/network.py
@@ -1,10 +1,12 @@
 from router import Router
 from content_store import ContentStore
 from flow_pusher import FlowPusher
+from topology import Topology
 
 class Network():
     def __init__(self):
-        self.topology = {}
+        self.topology = Topology()
+        self.topology.printTopology()
         self.router = Router(self.topology)
         self.contentStore = ContentStore(self.topology)
         self.flowPusher = FlowPusher(self.topology)
---
+++ network/topology.py
@@ -0,0 +1,43 @@
+import sys
+
+class Switch():
+    def __init__(self, name):
+        self.name = name
+        self.switches = []
+        self.hosts = []
+class Host():
+    def __init__(self, name):
+        self.name = name
+class Topology():
+    def __init__(self):
+        topology = [{'hosts': [1, 2, 3], 'switches': [2, 4, 5, 6]},
+            {'hosts': [], 'switches': [1, 3, 4]},
+            {'hosts': [4, 5, 6], 'switches': [2, 4, 7, 8]},
+            {'hosts': [], 'switches': [1, 2, 3, 6, 7]},
+            {'hosts': [7, 8, 9], 'switches': [1, 6]},
+            {'hosts': [], 'switches': [1, 4, 5]},
+            {'hosts': [], 'switches': [3, 4, 8]},
+            {'hosts': [10, 11, 12], 'switches': [3, 7]}]
+        self.switches = []
+        self.hosts = []
+
+        for idx, switch in enumerate(topology):
+            switchObject = Switch( 's' + str(idx + 1) )
+            switch['switchObject'] = switchObject
+            self.switches.append(switchObject)
+            for host in switch['hosts']:
+                hostObject = Host( 'h' + str(host))
+                self.hosts.append(hostObject)
+                switchObject.hosts.append(hostObject)
+
+        for switch in topology:
+            for neighbor in switch['switches']:
+                switch['switchObject'].switches.append(topology[neighbor - 1]['switchObject'])
+
+    def printTopology(self):
+        sys.stderr.write('Current Topology:\n')
+        for switch in self.switches:
+            sys.stderr.write(switch.name + ': \n')
+            sys.stderr.write('\tswitches: ' + ', '.join([s.name for s in switch.switches]) + '\n')
+            sys.stderr.write('\thosts: ' + ', '.join([h.name for h in switch.hosts]) + '\n')
+            sys.stderr.write('\n')
